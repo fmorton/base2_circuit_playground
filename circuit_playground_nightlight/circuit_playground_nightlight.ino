cc7241c768512783f44440b628e618eceb4cb847
Frank Morton <fmorton@base2inc.com>
2017-02-28 22:47:29 -0400

    add circuit_playground_nightlight rgb_colors support

--- circuit_playground_nightlight/circuit_playground_nightlight.ino
+++ circuit_playground_nightlight/circuit_playground_nightlight.ino
@@ -4,7 +4,7 @@ const int min_brightness = 0;
 const int max_brightness = 35;
 const int brightness_delay = 80;
 
-int current_color = -1;
+int current_color = 0;
 int colors[][3] = { { 255, 0, 0 }, { 0, 0, 223 }, { 0, 223, 0 }, { 169, 223, 20 }, { 20, 217, 223 }, { 20, 33, 223 } };
 int colors_count = sizeof(colors[0]) - 1;
 
@@ -13,7 +13,11 @@ void setup() {
   CircuitPlayground.clearPixels();
 }
 
-void set_color(int red, int green, int blue, int brightness, int brightness_interval) {
+void set_color(int *rgb_colors, int brightness, int brightness_interval) {
+  int red = rgb_colors[0];
+  int green = rgb_colors[1];
+  int blue = rgb_colors[2];
+
   CircuitPlayground.setBrightness(brightness);
 
   for(int pixel=0;pixel<10;++pixel) {
@@ -24,14 +28,14 @@ void set_color(int red, int green, int blue, int brightness, int brightness_inte
 }
 
 int change_colors(int color) {
+  int *rgb_colors = colors[color];
+
+  for(int brightness=min_brightness;brightness<=max_brightness;++brightness) set_color(rgb_colors, brightness, brightness_delay);
+  for(int brightness=max_brightness;brightness>=min_brightness;--brightness) set_color(rgb_colors, brightness, brightness_delay);
+
   if(++color > colors_count) color = 0;
 
-  int red = colors[color][0];
-  int green = colors[color][1];
-  int blue = colors[color][2];
-
-  for(int brightness=min_brightness;brightness<=max_brightness;++brightness) set_color(red, green, blue, brightness, brightness_delay);
-  for(int brightness=max_brightness;brightness>=min_brightness;--brightness) set_color(red, green, blue, brightness, brightness_delay);
+  int *to_rgb_colors = colors[color];
 
   return(color);
 }
