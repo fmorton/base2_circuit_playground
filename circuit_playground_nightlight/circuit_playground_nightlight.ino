db7d7d27ab75192fa47701aa60868b0d1caa76c2
Frank Morton <fmorton@base2inc.com>
2017-03-01 14:41:39 -0400

    add circuit_playground_nightlight transition_colors support

--- circuit_playground_nightlight/circuit_playground_nightlight.ino
+++ circuit_playground_nightlight/circuit_playground_nightlight.ino
@@ -1,8 +1,9 @@
 #include <Adafruit_CircuitPlayground.h>
 
-const int min_brightness = 0;
+const int min_brightness = 3;
 const int max_brightness = 35;
 const int brightness_delay = 80;
+const int transition_delay = 5;
 
 int current_color = 0;
 int colors[][3] = { { 255, 0, 0 }, { 0, 0, 223 }, { 0, 223, 0 }, { 169, 223, 20 }, { 20, 217, 223 }, { 20, 33, 223 } };
@@ -13,11 +14,7 @@ void setup() {
   CircuitPlayground.clearPixels();
 }
 
-void set_color(int *rgb_colors, int brightness, int brightness_interval) {
-  int red = rgb_colors[0];
-  int green = rgb_colors[1];
-  int blue = rgb_colors[2];
-
+void set_color(int red, int green, int blue, int brightness, int brightness_interval) {
   CircuitPlayground.setBrightness(brightness);
 
   for(int pixel=0;pixel<10;++pixel) {
@@ -27,15 +24,43 @@ void set_color(int *rgb_colors, int brightness, int brightness_interval) {
   delay(brightness_interval);
 }
 
+int next_transition_color(int from, int to) {
+  if (from < to) return(++from);
+  if (from > to) return(--from);
+  return(from);
+}
+
+void transition_colors(int *from_rgb_colors, int *to_rgb_colors) {
+  int red = from_rgb_colors[0];
+  int green = from_rgb_colors[1];
+  int blue = from_rgb_colors[2];
+
+  while ((red != to_rgb_colors[0]) || (green != to_rgb_colors[1]) || (blue != to_rgb_colors[2])) {
+    red = next_transition_color(red, to_rgb_colors[0]);
+    green = next_transition_color(green, to_rgb_colors[1]);
+    blue = next_transition_color(blue, to_rgb_colors[2]);
+
+    set_color(red, green, blue, min_brightness, transition_delay);
+  }
+}
+
 int change_colors(int color) {
   int *rgb_colors = colors[color];
 
-  for(int brightness=min_brightness;brightness<=max_brightness;++brightness) set_color(rgb_colors, brightness, brightness_delay);
-  for(int brightness=max_brightness;brightness>=min_brightness;--brightness) set_color(rgb_colors, brightness, brightness_delay);
+  int red = rgb_colors[0];
+  int green = rgb_colors[1];
+  int blue = rgb_colors[2];
+
+  for(int brightness=min_brightness;brightness<=max_brightness;++brightness) set_color(red, green, blue, brightness, brightness_delay);
+  for(int brightness=max_brightness;brightness>=min_brightness;--brightness) set_color(red, green, blue, brightness, brightness_delay);
 
   if(++color > colors_count) color = 0;
 
-  int *to_rgb_colors = colors[color];
+  if(min_brightness > 0) {
+    int *to_rgb_colors = colors[color];
+
+    transition_colors(rgb_colors, to_rgb_colors);
+  }
 
   return(color);
 }
